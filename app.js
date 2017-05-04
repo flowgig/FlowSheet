b4b93762e9caa25d3844d2094d4e3cfaf5b72e71
benjamindehli <superelg@gmail.com>
2017-05-04 21:37:25 +0200

    Add basic chord parser

--- app.js
+++ app.js
@@ -1,6 +1,48 @@
 var app = new Vue({
 	el: '#app',
 	data: {
-		
+		source: ''
+	},
+	computed: {
+		parsed: function(){
+			var parsedLines = [];
+			var commentRegex = /^\s*#.*/;
+			var isBetweenTabDirectives = false;
+			return this.getLines(this.source);
+		}
+	},
+	methods: {
+		getLines: function(source){
+			var lines = [];
+			source.split('\n').forEach(function(line) {
+				parsedLine = {
+					text: line,
+					chords: this.parseChord(line)
+				}
+				lines.push(parsedLine);
+			}.bind(this));
+			return lines;
+		},
+		parseChord: function(line) {
+			var regex = /\[([a-zA-Z0-9#+\/]*)\]/g;
+			var m;
+			var chords = [];
+			while ((m = regex.exec(line)) !== null) {
+			    // This is necessary to avoid infinite loops with zero-width matches
+			    if (m.index === regex.lastIndex) {
+			    	regex.lastIndex++;
+			    }
+			    var chord = {};
+			    // The result can be accessed through the `m`-variable.
+			    m.forEach(function(match, groupIndex) {
+			    	if (groupIndex == 0) {chord.markup = match}
+			    	if (groupIndex == 1) {chord.name = match}
+			    });
+			    chord.position = m.index;
+			    chords.push(chord);
+			}
+			return chords;
+		}
+
 	}
 });
