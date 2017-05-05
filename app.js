304de588ea993f43d3e8725323c4d396cf0b36e3
benjamindehli <superelg@gmail.com>
2017-05-05 22:47:26 +0200

    Adjust logic for chord line

--- app.js
+++ app.js
@@ -12,39 +12,49 @@ var app = new Vue({
 		getLines: function(source){
 			var lines = [];
 			source.split('\n').forEach(function(line) {
-				parsedLine = {
-					lyricString: line,
-					chords: this.parseChord(line)
-				}
-				var index = 0;
-				var chordline = "";
-				if(parsedLine.chords.length){
-					while (index < line.length){
-						var isChord = false;
-						parsedLine.chords.forEach(function(chord){
-							if (chord.position == index){
-								chordline += chord.name;
-								index += chord.name.length;
-								isChord = true;
-							}
-						});
-						if (!isChord){
-							chordline += "&nbsp;";
-							index++;
-						}
-					}
-				}else{
-					chordline = "&nbsp;";
-				}
-				parsedLine.chordString = chordline;
-				parsedLine.chords.forEach(function(chord){
-					parsedLine.lyricString = parsedLine.lyricString.replace(chord.markup, "");
-				});
+				var parsedLine = {
+					lyricString: this.parseLyricLine(line),
+					chordString: this.parseChordLine(line)
+				};
 				lines.push(parsedLine);
 			}.bind(this));
 			return lines;
 		},
-		parseChord: function(line) {
+		parseLyricLine: function(line){
+			var chords = this.getChords(line)
+			if (chords.length){
+				chords.forEach(function(chord){
+					line = line.replace(chord.markup, "");
+				});
+			}
+			return line;
+		},
+		parseChordLine: function(line){
+			var chords = this.getChords(line)
+			var index = 0;
+			var chordline = "";
+			if(chords.length){
+				charactersRemoved = 0;
+				while (index < line.length){
+					var isChord = false;
+					chords.forEach(function(chord){
+						if (chord.position == index){
+							chordline += chord.name;
+							index += chord.markup.length + chord.name.length;
+							isChord = true;
+						}
+					});
+					if (!isChord){
+						chordline += "&nbsp;";
+						index++;
+					}
+				}
+			}else{
+				chordline = "&nbsp;";
+			}
+			return chordline;
+		},
+		getChords: function(line) {
 			var regex = /\[([a-zA-Z0-9#+\/]*)\]/g;
 			var m;
 			var chords = [];
