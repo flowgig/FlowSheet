dc07410ba5279d5ed6fb2479356414622f08efea
benjamindehli <superelg@gmail.com>
2017-05-13 23:32:32 +0200

    Add component for modals

---
+++ src/components/Modals.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <edit-song-info></edit-song-info>
+  </div>
+
+  
+</template>
+
+<script>
+import EditSongInfo from './modals/EditSongInfo.vue'
+ export default {
+  name: 'modals',
+  data () {
+    return {
+      test: 'testdata'
+    }
+  },
+  components: {
+    editSongInfo: EditSongInfo
+  },
+  created (){
+  },
+  computed: {},
+  methods: {
+  }
+}
+</script>
+
+<style lang="css">
+
+</style>
---
+++ src/components/modals/EditSongInfo.vue
@@ -0,0 +1,31 @@
+<template>
+  <div v-html="htmlContent">
+    
+  </div>
+</template>
+
+<script>
+  import * as quark from 'quark-gui';
+  export default {
+    name: 'editSongInfo',
+    data () {
+      return {
+        test: 'testdata',
+        htmlContent: quark.Molecules.Messaging.Modal.getModule({
+        id: 'editSongInfo',
+        title: 'testmodaltittel',
+        content: `<input v-model='$parent.$parent.metaInfo.title' name=''><button>close</button>`
+      })
+      }
+    },
+    created (){
+    },
+    computed: {},
+    methods: {
+    }
+  }
+</script>
+
+<style lang="css">
+
+</style>
