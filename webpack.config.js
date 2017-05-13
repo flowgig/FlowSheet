85ef02e27b969b13d5ef9d45245381e03a4e1a2f
benjamindehli <superelg@gmail.com>
2017-05-13 23:16:52 +0200

    Add webpack loaders for fonts and svg files

--- webpack.config.js
+++ webpack.config.js
@@ -5,10 +5,10 @@ var ExtractTextPlugin = require('extract-text-webpack-plugin');
 const cssIdentifier = process.env.NODE_ENV === 'production' ? '[hash:base64:10]' : '[path][name]---[local]';
 
 const sassLoader = process.env.NODE_ENV === 'production'
-  ? ExtractTextPlugin.extract({
-      use: 'css-loader?minimize!sass-loader?localIdentName=' + cssIdentifier
-    })
-  :   ['style-loader', 'css-loader?localIdentName=' + cssIdentifier, 'sass-loader'];
+? ExtractTextPlugin.extract({
+  use: 'css-loader?minimize!sass-loader?localIdentName=' + cssIdentifier
+})
+:   ['style-loader', 'css-loader?localIdentName=' + cssIdentifier, 'sass-loader'];
 
 module.exports = {
   entry: './src/main.js',
@@ -19,11 +19,11 @@ module.exports = {
   },
   module: {
     rules: [
-      {
-        test: /\.vue$/,
-        loader: 'vue-loader',
-        options: {
-          loaders: {
+    {
+      test: /\.vue$/,
+      loader: 'vue-loader',
+      options: {
+        loaders: {
             // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
             // the "scss" and "sass" values for the lang attribute to the right configs here.
             // other preprocessors should work out of the box, no loader config like this necessary.
@@ -34,7 +34,7 @@ module.exports = {
               fallback: 'vue-style-loader' // <- this is a dep of vue-loader, so no need to explicitly install if using npm3
             })
           },
-         
+
           // other vue-loader options go here
         }
       },
@@ -54,29 +54,41 @@ module.exports = {
         test: /\.scss$/,
         use: sassLoader,
         exclude: '/node_modules/'
+      },
+      { 
+        test: /\.(ttf|eot)(\?v=[0-9]\.[0-9]\.[0-9])?$/, 
+        use: "file-loader?name=fonts/[hash:12].[ext]" 
+      }, 
+      { 
+        test: /\.svg(\?v=[0-9]\.[0-9]\.[0-9])?$/, 
+        use: "svg-url-loader?noquotes&limit=1024&prefix=svg/[hash:12].[ext]"
+      },
+      {  
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,  
+        use: "url-loader?limit=10000&mimetype=application/font-woff&name=fonts/[hash:12].[ext]"  
       }
-    ]
-  },
-  plugins: [
+      ]
+    },
+    plugins: [
     new ExtractTextPlugin("style.css")
-  ],
-  resolve: {
-    alias: {
-      'vue$': 'vue/dist/vue.esm.js'
-    }
-  },
-  devServer: {
-    historyApiFallback: true,
-    noInfo: true
-  },
-  performance: {
-    hints: false
-  },
-  devtool: '#eval-source-map'
-}
+    ],
+    resolve: {
+      alias: {
+        'vue$': 'vue/dist/vue.esm.js'
+      }
+    },
+    devServer: {
+      historyApiFallback: true,
+      noInfo: true
+    },
+    performance: {
+      hints: false
+    },
+    devtool: '#eval-source-map'
+  }
 
-if (process.env.NODE_ENV === 'production') {
-  module.exports.devtool = '#source-map'
+  if (process.env.NODE_ENV === 'production') {
+    module.exports.devtool = '#source-map'
   // http://vue-loader.vuejs.org/en/workflow/production.html
   module.exports.plugins = (module.exports.plugins || []).concat([
     new webpack.DefinePlugin({
@@ -93,5 +105,5 @@ if (process.env.NODE_ENV === 'production') {
     new webpack.LoaderOptionsPlugin({
       minimize: true
     })
-  ])
+    ])
 }
