5e6b29687511b15e7ce6f1bbed760533dd82679e
Benjamin Dehli <benjamin@arkitektum.no>
2017-10-22 22:28:14 +0200

    Adjust public path for webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist/',
+    publicPath: process.env.NODE_ENV === 'production' ? '' : 'dist/',
     filename: 'build.js'
   },
   module: {
