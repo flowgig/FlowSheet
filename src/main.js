2faaab5a90c25488c166dc28ea43ce5b74adeccf
Benjamin Dehli <benjamin@arkitektum.no>
2017-10-22 22:51:22 +0200

    Initialize quarkGUI

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './components/App.vue'
 import Style from './style/main.scss';
+import * as quark from 'quark-gui';
+
 
 new Vue({
   el: '#app',
   render: h => h(App)
 })
+
+quark.Init.default();
